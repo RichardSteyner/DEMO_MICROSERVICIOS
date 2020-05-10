a17c057f6acc2b0b5a52901775c9763af7e4d33a
Steyner <steyner.urupeque.s@gmail.com>
2020-05-10 13:16:24 -0500

    add config server a service customer

--- service-customer/src/main/java/com/codifacil/servicecustomer/ServiceCustomerApplication.java
+++ service-customer/src/main/java/com/codifacil/servicecustomer/ServiceCustomerApplication.java
@@ -2,8 +2,9 @@ package com.codifacil.servicecustomer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ServiceCustomerApplication {
 
 	public static void main(String[] args) {
